0a67d9379802b771c91dd4afb52b629c497c91f6
jarod-empirys <jarod.couprie@empirys.com>
2024-04-15 10:55:52 +0200

    dev init

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello from TypeScript Express!");
+    res.send("Bienvenue sur SIRH");
 });
 
 app.listen(port, () => {
