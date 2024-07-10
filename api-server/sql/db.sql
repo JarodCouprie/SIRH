b896e5c2f3e129d86a76416cdc4f284064161956
Hyokkin11 <ethan.collignon.1102@gmail.com>
2024-07-10 13:01:30 +0200

    feat(expense): merge

--- api-server/sql/db.sql
+++ api-server/sql/db.sql
@@ -219,24 +219,3 @@ CREATE TABLE belong_team_service
     FOREIGN KEY (id_service) REFERENCES service (id)
 );
 
-INSERT INTO users(firstName, lastName, email, password, address, nationality, role, iban)
-VALUES ("Super", "Admin", "admin@admin.com", "$2b$10$e5Kv7sv9QlCdFGQBYTPBguSx3.Ogqbgq8DSy4JcAo5Y3ubYhdSQo6",
-        "admin address", "admin nationaly", "ROLE_ADMIN", "admin iban"),
-       ("Simple", "User", "simple@user.com", "$2b$10$e5Kv7sv9QlCdFGQBYTPBguSx3.Ogqbgq8DSy4JcAo5Y3ubYhdSQo6",
-        "user address", "user nationaly", "ROLE_USER", "user iban");
-
-INSERT INTO expense(type,amount,motivation,status,id_owner,id_validator, facturationDate)
-VALUES ("TRAVEL", 300, "Voyage d'affaire", "WAITING", 2, null, '2024-03-11' ),
-       ("COMPENSATION", 50, "Indemnisation", "REFUNDED", 2, 1, '2023-05-19'),
-       ("FOOD", 100, "Repas pro", "WAITING", 1, null, '2024-04-15'),
-       ("HOUSING", 149, "Hotel", "WAITING", 1, null, '2024-04-01'),("TRAVEL", 300, "Voyage d'affaire", "WAITING", 2, null, '2024-03-11' ),
-       ("COMPENSATION", 50, "Compensation accident", "REFUNDED", 2, 1, '2023-12-13'),
-       ("FOOD", 102, "Repas pro", "WAITING", 1, null, '2024-04-15'),
-       ("HOUSING", 149, "Hotel", "WAITING", 1, null, '2024-04-01'),("TRAVEL", 300, "Voyage d'affaire", "WAITING", 2, null, '2024-03-11' ),
-       ("COMPENSATION", 89, "Prime", "REFUNDED", 2, 1, '2023-12-13'),
-       ("FOOD", 20.99, "Restauration en déplacement", "NOT_REFUNDED", 1, null, '2024-04-15'),
-       ("HOUSING", 149, "Hotel", "WAITING", 1, null, '2024-05-01'),("TRAVEL", 300, "Voyage d'affaire", "WAITING", 2, null, '2024-03-11' ),
-       ("COMPENSATION", 50, "Indemnisation", "REFUNDED", 2, 1, '2023-05-13'),
-       ("FOOD", 34.99, "Repas d'affaire", "WAITING", 1, null, '2024-04-15'),
-       ("FOOD", 34.99, "Repas de test", "REFUNDED", 1, null, '2024-04-15'),
-       ("HOUSING", 99, "AirBNB", "NOT_REFUNDED", 1, null, '2024-05-01');
