afe50ee22e087954efd3924b51c526f3c1f1754c
Ethan-C11 <Ethan-C11@ethan.collignon.1102@gmail.com>
2024-08-19 17:58:41 +0200

    feat(expense): setting up jest test

--- api-server/src/test/expense.test.ts
+++ api-server/src/test/expense.test.ts
@@ -1,3 +1,12 @@
+import { Request } from "express";
+import { DemandRepository } from "../resources/demand/DemandRepository";
+import { DemandService } from "../resources/demand/DemandService";
+import { ControllerResponse } from "../common/helper/ControllerResponse";
+import { DemandDTO } from "../resources/demand/dto/DemandDTO";
+import { Expense, ExpenseType } from "../common/model/Expense";
+import { ExpenseService } from "../resources/expense/ExpenseService";
+import { ExpenseListDTO } from "../resources/expense/dto/ExpenseListDTO";
+
 jest.mock("../resources/user/UserService.js");
 jest.mock("../resources/expense/ExpenseRepository.js");
 jest.mock("../resources/expense/ExpenseService.js", () => ({
@@ -6,3 +15,46 @@ jest.mock("../resources/expense/ExpenseService.js", () => ({
   updateUserDays: jest.fn(),
 }));
 jest.mock("../common/helper/Logger.js");
+
+describe("getAllExpensesByType", () => {
+  let req: Partial<Request>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    req = {
+      query: {
+        pageSize: "10",
+        pageNumber: "1",
+        type: "FOOD",
+      },
+    };
+  });
+
+  test("should return expenses with a specific type", async () => {
+    const expense = [
+      {
+        id: 45,
+        startDate: new Date("2024-07-22"),
+        endDate: new Date("2024-07-25"),
+        motivation: "qsdfqsdf",
+        created_at: new Date("2024-07-01"),
+        status: "WAITING",
+        type: ExpenseType.FOOD,
+        number_day: 3,
+        idOwner: 1,
+        idValidator: 1,
+      },
+    ] as Expense[];
+    const expenseCount = 1;
+
+
+
+    //const response = await ExpenseService.getExpensesValuesByUserId().(req as Request);
+
+    expect(response).toEqual(
+      new ControllerResponse(200, "", {
+        totalData: expenseCount,
+        list: expense.map((expense) => new ExpenseListDTO(expense)),
+      }),
+    );
+  });
