a17cba672d6220d3469cbecc3b7b8cdd5d4c6c33
Hyokkin11 <ethan.collignon.1102@gmail.com>
2024-08-20 14:42:08 +0200

    feat(expense): wip tests

--- api-server/src/test/expense.test.ts
+++ api-server/src/test/expense.test.ts
@@ -1,33 +1,62 @@
-import { test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { Expense, ExpenseStatus, ExpenseType } from "../common/model/Expense";
-import { ExpenseRepository } from "../resources/expense/ExpenseRepository";
-import { undefined } from "zod";
 
 // https://vitest.dev/api/
 
-test("Add expense to first user", async () => {
-  const expense: Expense = {
-    amount: 500,
-    created_at: new Date(),
-    facturation_date: new Date(),
-    id: "1",
-    id_owner: 1,
-    id_validator: 0,
-    justification: "justif",
-    motivation: "motiv",
-    status: ExpenseStatus.WAITING,
-    type: ExpenseType.FOOD,
-  };
-  const createExpense = await ExpenseRepository.createExpenseDemand(expense);
-  const createdExpense: Expense = await ExpenseRepository.getExpenseDemand("1");
-});
+describe("Expense Model", () => {
+  test("Test Expense Model With Only Mandatory Attributes Should Be Instance Of Expense", () => {
+    const expense = new Expense(
+      "1",
+      ExpenseType.FOOD,
+      500,
+      "motivation",
+      new Date(),
+      new Date(),
+      ExpenseStatus.WAITING,
+      0,
+      0,
+      "justification",
+    );
 
-test("Get first 10 Expenses", async () => {
-  const offset = "0";
-  const limit = "10";
+    expect(expense).toBeInstanceOf(Expense);
+  });
 
-  const expenses: Expense[] = await ExpenseRepository.getExpensesValues(
-    +offset,
-    +limit,
-  );
+  test("Test Expense Model With All Attributes Should Be Instance Of Expense", () => {
+    const expense = new Expense(
+      "1",
+      ExpenseType.FOOD,
+      500,
+      "motivation",
+      new Date(),
+      new Date(),
+      ExpenseStatus.WAITING,
+      0,
+      0,
+      "justification",
+      "firstname",
+      "lastname",
+      new Date(),
+      "randomkey",
+    );
+
+    expect(expense).toBeInstanceOf(Expense);
+  });
+
+  test("Test Incorrect Expense Model With Only Mandatory Attributes Should Not Be Instance Of Expense", () => {
+    const notExpense = {
+      amount: 500,
+      incorrect_attribute: "this is not supposed to be here",
+      created_at: new Date(),
+      facturation_date: new Date(),
+      id: "1",
+      id_owner: 0,
+      id_validator: 0,
+      motivation: "motivation",
+      status: ExpenseStatus.WAITING,
+      type: ExpenseType.FOOD,
+      incorrect_number: 1,
+    };
+
+    expect(notExpense).not.toBeInstanceOf(Expense);
+  });
 });
