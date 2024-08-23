f4dde05fcfd634bd780bb1ca6d7e78b61047545a
Hyokkin11 <ethan.collignon.1102@gmail.com>
2024-08-23 15:53:56 +0200

    feat(expense): wip tests expenses

--- api-server/src/test/expense.test.ts
+++ api-server/src/test/expense.test.ts
@@ -1,6 +1,10 @@
 import { describe, expect, test } from "vitest";
 import { Expense } from "../common/model/Expense";
-import { ExpenseListDTO } from "../resources/expense/dto/ExpenseListDTO";
+import {
+  ExpenseInvalidation,
+  ExpenseListDTO,
+  ExpenseValidation,
+} from "../resources/expense/dto/ExpenseListDTO";
 import { ExpenseAmountDateAndStatusDTO } from "../resources/expense/dto/ExpenseAmountDateAndStatusDTO";
 import { ExpenseType } from "../common/enum/ExpenseType";
 import { ExpenseStatus } from "../common/enum/ExpenseStatus";
@@ -237,5 +241,97 @@ describe("ExpenseAmountDateAndStatusDTO", () => {
 
     expect(expense.amount).not.toBe(500);
     expect(expense.status).not.toBe(ExpenseStatus.WAITING);
+  });
+});
+
+describe("ExpenseValidation", () => {
+  test("Test ExpenseValidation  Should Be Instance Of Expense And Values Equals", () => {
+    const expenseValidation = new ExpenseValidation(1, 1);
+    expenseValidation.validated_at = new Date("2099")
+      .toISOString()
+      .split("Z")[0]
+      .replace("T", " ")
+      .split(".")[0];
+
+    const dateToTest = new Date("2099")
+      .toISOString()
+      .split("Z")[0]
+      .replace("T", " ")
+      .split(".")[0];
+
+    expect(expenseValidation.id).toBe(1);
+    expect(expenseValidation.id_validator).toBe(1);
+    expect(expenseValidation.validated_at).toStrictEqual(dateToTest);
+    expect(expenseValidation).toBeInstanceOf(ExpenseValidation);
+  });
+
+  test("Test Incorrect ExpenseValidation  Should Not Be Instance Of Expense", () => {
+    const notExpenseValidation = {
+      amount: 500,
+      incorrect_attribute: "this is not supposed to be here",
+      created_at: new Date(),
+    };
+
+    expect(notExpenseValidation).not.toBeInstanceOf(ExpenseValidation);
+  });
+
+  test("Test Correct ExpenseValidation  False Value Should Not Be Equal", () => {
+    const expenseValidation = new ExpenseValidation(1, 1);
+    expenseValidation.validated_at = new Date("2099")
+      .toISOString()
+      .split("Z")[0]
+      .replace("T", " ")
+      .split(".")[0];
+
+    expect(expenseValidation.id).not.toBe(2);
+    expect(expenseValidation.id_validator).not.toBe(3);
+    expect(expenseValidation.validated_at).not.toBe(new Date("1999"));
+  });
+});
+
+describe("ExpenseInvalidation", () => {
+  test("Test ExpenseInvalidation  Should Be Instance Of Expense And Values Equals", () => {
+    const expenseInvalidation = new ExpenseInvalidation(1, "justification", 1);
+    expenseInvalidation.validated_at = new Date("2099")
+      .toISOString()
+      .split("Z")[0]
+      .replace("T", " ")
+      .split(".")[0];
+
+    const dateToTest = new Date("2099")
+      .toISOString()
+      .split("Z")[0]
+      .replace("T", " ")
+      .split(".")[0];
+
+    expect(expenseInvalidation.id).toBe(1);
+    expect(expenseInvalidation.id_validator).toBe(1);
+    expect(expenseInvalidation.validated_at).toStrictEqual(dateToTest);
+    expect(expenseInvalidation.justification).toBe("justification");
+    expect(expenseInvalidation).toBeInstanceOf(ExpenseInvalidation);
+  });
+
+  test("Test Incorrect ExpenseInvalidation Should Not Be Instance Of Expense", () => {
+    const notExpenseInvalidation = {
+      amount: 500,
+      incorrect_attribute: "this is not supposed to be here",
+      created_at: new Date(),
+    };
+
+    expect(notExpenseInvalidation).not.toBeInstanceOf(ExpenseInvalidation);
+  });
+
+  test("Test Correct ExpenseInvalidation False Value Should Not Be Equal", () => {
+    const expenseInvalidation = new ExpenseInvalidation(1, "justification", 1);
+    expenseInvalidation.validated_at = new Date("2099")
+      .toISOString()
+      .split("Z")[0]
+      .replace("T", " ")
+      .split(".")[0];
+
+    expect(expenseInvalidation.id).not.toBe(2);
+    expect(expenseInvalidation.id_validator).not.toBe(3);
+    expect(expenseInvalidation.validated_at).not.toBe(new Date("1999"));
+    expect(expenseInvalidation.justification).not.toBe(new Date("test"));
   });
 });
