d2aef7f24d64ba2dee7bc895aa7d6bc2391e798d
Theo <theoroblin01@outlook.fr>
2024-09-02 09:40:26 +0200

    feat(dev): add agency data

--- api-server/sql/data.sql
+++ api-server/sql/data.sql
@@ -45,3 +45,44 @@ VALUES ("TRAVEL", 300, "Voyage d'affaire", "WAITING", 2, null, '2024-03-11'),
 INSERT INTO `demand`(`id`, `start_date`, `end_date`, `motivation`, `created_at`, `status`, `type`, `number_day`,
                      `id_owner`)
 VALUES ('1', CURRENT_DATE, CURRENT_DATE, 'je suis motive', CURRENT_DATE, 'WAITING', 'CA', 1, '1');
+INSERT INTO address (id, street, streetNumber, locality, zipcode, lat, lng) VALUES
+                                                                                (101, 'Rue aux arènes', '86', 'Metz', '57000', 49.1068, 6.1764),
+                                                                                (102, 'Rue de la République', '45', 'Lyon', '69002', 45.7640, 4.8357),
+                                                                                (103, 'Boulevard Michelet', '90', 'Marseille', '13008', 43.2965, 5.3698),
+                                                                                (104, 'Rue Nationale', '12', 'Lille', '59800', 50.6292, 3.0573),
+                                                                                (105, 'Avenue Jean Jaurès', '34', 'Toulouse', '31000', 43.6047, 1.4442);
+
+INSERT INTO agency (id, label, id_address) VALUES
+                                               (1, 'Metz Numérique School', 101),
+                                               (2, 'Agence Lyon Nord', 102),
+                                               (3, 'Agence Marseille Est', 103),
+                                               (4, 'Agence Lille Ouest', 104),
+                                               (5, 'Agence Toulouse Sud', 105);
+
+INSERT INTO service (id, label, minimum_users, id_user_lead_service, id_agency) VALUES
+                                                                                    (1, 'Service Educatif', 5, 1, 1),
+                                                                                    (2, 'Service IT', 10, 1, 2),
+                                                                                    (3, 'Service Commercial', 8, 1, 3),
+                                                                                    (4, 'Service Marketing', 6, 2, 4),
+                                                                                    (5, 'Service Logistique', 7, 2, 5);
+
+INSERT INTO team (id, label, minimum_users, id_user_lead_team, id_service) VALUES
+                                                                               (1, 'Équipe Intervenant', 3, 1, 1),
+                                                                               (2, 'Équipe Support IT', 5, 1, 2),
+                                                                               (3, 'Équipe Vente', 4, 2, 3),
+                                                                               (4, 'Équipe SEO', 4, 2, 4),
+                                                                               (5, 'Équipe Transport', 6, 2, 5);
+
+INSERT INTO belong_team (id_team, id_user) VALUES
+                                               (1, 1),
+                                               (1, 2),
+                                               (2, 1),
+                                               (2, 2),
+                                               (3, 1),
+                                               (3, 2),
+                                               (4, 1),
+                                               (4, 2),
+                                               (5, 1),
+                                               (5, 2);
+
+
